c12fe85f6056c3aa175151bfa5b82c1da6aebcc9
Andy Gallagher <andy.gallagher@theguardian.com>
2015-03-20 08:34:45 +0000

    Hello world is now working, with a template integrated to rest of UI. Pycharm gubbins commit.

--- helloworld/views.py
+++ helloworld/views.py
@@ -1,5 +1,8 @@
+from portal.generic.baseviews import ClassView
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-  return HttpResponse(content="Hello world!",content_type="text/plain",status=200)
+  #return HttpResponse(content="Hello world!",content_type="text/plain",status=200)
+  return render(request,"helloworld.html")
 

6857c7af1820d02162c0096196a055343dfe6e12
Andy Gallagher <andy.gallagher@theguardian.com>
2015-05-26 10:01:30 +0100

    develop branch - work on making a csv report work, plus merges from master branch

--- gnmsyndication/urls.py
+++ gnmsyndication/urls.py
@@ -6,6 +6,7 @@ urlpatterns = patterns(
     'portal.plugins.gnmsyndication.views',
     url(r'^stats/data/platforms_by_day$', views.platforms_by_day),
     url(r'^stats/assets_by_day/(\d{1,2}/\d{1,2}/\d{4})$', views.assets_by_day),
+    url(r'^stats/report/csv$', views.csv_report),
     url(r'^stats/$', views.index),
     url(r'^stats$', redirect_to, {'url': 'stats/'}),
     url(r'^$', redirect_to, {'url': 'stats/' }),

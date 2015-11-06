736a3c0cc77adcd1130ad2de622b9ca34bcecbe8
Andy Gallagher <andy.gallagher@theguardian.com>
2015-11-06 12:34:33 +0000

    Adding RPM build gubbins

--- buildrpms.sh
+++ buildrpms.sh
@@ -3,3 +3,7 @@
 tar cv gnmpurgemeister | gzip > ${HOME}/rpmbuild/gnmpurgemeister.tar.gz
 rpmbuild -bb gnmpurgemeister.spec
 mv ${HOME}/rpmbuild/RPMS/noarch/portal-purgemeister* .
+
+tar cv gnmplutostats | gzip > ${HOME}/rpmbuild/gnmplutostats.tar.gz
+rpmbuild -bb gnmplutostats.spec
+mv ${HOME}/rpmbuild/RPMS/noarch/portal-plutostats* .

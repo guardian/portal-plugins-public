952d4be2f3ef0b980ad20e4f423fe457a00928f1
David Allison <david.allison@theguardian.com>
2015-12-17 17:36:07 +0000

    Added code which only displays the menu option on a collection page when at least one of the items in the collection is in the archive.

--- gnmawsgr/__init__.py
+++ gnmawsgr/__init__.py
@@ -186,9 +186,55 @@ class GNMAWSGRCollectionGearboxMenuPlugin(Plugin):
         self.plugin_guid = "05eed808-5c6b-42a7-88a1-0336bcf790d1"
         log.debug("Initiated GNMAWSGRCollectionGearboxMenuPlugin")
 
-    def return_string(self, tagname, *args):
-        display = 1
+    def recurse_for_field(self, mdkey, data):
+        for f in data:
+            if f['name'] == mdkey:
+                return map(lambda x: x['value'], f['value'])
 
+    def recurse_group(self, mdkey, data):
+        self.recurse_for_field(data['field'])
+        self.recurse_group(data['group'])
+
+    def metadataValueForKey(self, mdkey, meta):
+        for item_data in meta:
+            for ts in item_data['metadata']['timespan']:
+                rtn = self.recurse_for_field(mdkey, ts['field'])
+                if rtn is not None:
+                    return rtn
+
+                for g in ts['group']:
+                    rtn = self.recurse_group(mdkey,g)
+                    if rtn is not None:
+                        return rtn
+
+    def _find_group(self,groupname,meta):
+        if not 'group' in meta:
+            return None
+
+        for g in meta['group']:
+            if g['name'] == groupname:
+                return g
+            self._find_group(groupname,g)
+
+    def metadataValueInGroup(self, groupname, mdkey, meta):
+        for item_data in meta:
+            for ts in item_data['metadata']['timespan']:
+                group = self._find_group(groupname, ts)
+                if group is None:
+                    raise ValueError("Could not find group {0}".format(groupname))
+                for f in group['field']:
+                    if f['name'] == mdkey:
+                        rtn = map(lambda x: x['value'],f['value'])
+                        if len(rtn)==1:
+                            return rtn[0]
+                        else:
+                            return rtn
+        raise ValueError("Could not find metadata key {0}".format(mdkey))
+
+    def return_string(self, tagname, *args):
+        display = 0
+
+        from portal.vidispine.iitem import ItemHelper
         from pprint import pprint
 
         context = args[1]
@@ -198,10 +244,23 @@ class GNMAWSGRCollectionGearboxMenuPlugin(Plugin):
         #pprint(content[0].getId())
 
         for data in content:
+
+            ith = ItemHelper()
+            res = ith.getItemMetadata(data.getId())
             pprint(data.getId())
+            print 'ran'
+            try:
+                test_value = self.metadataValueInGroup('ExternalArchiveRequest','gnm_external_archive_external_archive_status',res['item'])
+            except:
+                print 'An error broke the call'
+            print 'Got past suspect code'
+            if test_value == archive_test_value:
+                display = 1
+                print 'Found'
+            print 'Got to end'
 
         if display == 1:
-            return {'guid':self.plugin_guid, 'template':'collection_gearbox_menu.html', 'context' : {'itemid':'', 'res':content} }
+            return {'guid':self.plugin_guid, 'template':'collection_gearbox_menu.html', 'context' : {'itemid':'', 'res':''} }
 
 GNMAWSGRCollectionpluginblock = GNMAWSGRCollectionGearboxMenuPlugin()
 

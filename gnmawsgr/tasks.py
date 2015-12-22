44da85e2c7b4b27ed6489f848b6acee638b449c2
David Allison <david.allison@theguardian.com>
2015-12-22 18:11:25 +0000

    Adding code which sends test messages to the Portal log file.

--- gnmawsgr/tasks.py
+++ gnmawsgr/tasks.py
@@ -1,10 +1,15 @@
 import celery
+import logging
+log = logging.getLogger(__name__)
+log.info('Testing logging outside Celery task')
+
+logger = celery.utils.log.get_task_logger(__name__)
 
 @celery.task
 def glacier_restore(itemid,path):
-    #print itemid
-    #print '\n'
-    #print path
+    logger.info("Testing logging inside Celery task")
+    logger.info("Item is "+itemid)
+    logger.info("Path is "+path)
     return 'Finished'
 
 

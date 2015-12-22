9a2c66dd1b2f680cff70cf797b5b2f7664485683
David Allison <david.allison@theguardian.com>
2015-12-22 18:17:04 +0000

    Adding code which attempts to connect to S3.

--- gnmawsgr/tasks.py
+++ gnmawsgr/tasks.py
@@ -7,7 +7,9 @@ logger = celery.utils.log.get_task_logger(__name__)
 
 @celery.task
 def glacier_restore(itemid,path):
-    logger.info("Testing logging inside Celery task")
+    from boto.s3.connection import S3Connection
+    conn = S3Connection('AKIAJXSAJRQLV3ERJLRA', 'g+3wrhtIvdylS2zh0xtnnogSiEWs4zLzPGoxytw6')
+    logger.info("Attempting to connect to S3")
     logger.info("Item is "+itemid)
     logger.info("Path is "+path)
     return 'Finished'

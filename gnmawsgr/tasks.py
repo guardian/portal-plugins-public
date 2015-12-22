bd0b7de69e893a378a1f2f99733a06948ec685f3
David Allison <david.allison@theguardian.com>
2015-12-22 18:32:47 +0000

    Adding code which attempts to start a restore job and then test if is working and send useful data to the log.

--- gnmawsgr/tasks.py
+++ gnmawsgr/tasks.py
@@ -7,11 +7,20 @@ logger = celery.utils.log.get_task_logger(__name__)
 
 @celery.task
 def glacier_restore(itemid,path):
+    import time
     from boto.s3.connection import S3Connection
     conn = S3Connection('AKIAJXSAJRQLV3ERJLRA', 'g+3wrhtIvdylS2zh0xtnnogSiEWs4zLzPGoxytw6')
     logger.info("Attempting to connect to S3")
-    logger.info("Item is "+itemid)
-    logger.info("Path is "+path)
+
+    bucket = conn.get_bucket('gnm-multimedia-deeparchive')
+    key = bucket.get_key(path)
+    key.restore(days=1)
+    logger.info("Attempting to restore "+path+" from S3")
+    time.sleep(20)
+    if key.ongoing_restore == True:
+        logger.info("Ongoing restore for "+path+" is working")
+    else:
+        logger.info("Ongoing restore for "+path+" is not working")
     return 'Finished'
 
 

645e925ac39c41ac4b6a440e2e5a4c757a29b071
Andy Gallagher <andy.gallagher@theguardian.com>
2016-01-06 10:41:39 +0000

    Wrapping task in a generic exception catching routine, to ensure that errors are logged into the log, into Sentry and also into the database model. Adding an option to delete rows from the database model (requires relevant permissions) bit of fiddling

--- gnmawsgr/urls.py
+++ gnmawsgr/urls.py
@@ -1,11 +1,14 @@
 from django.conf.urls.defaults import patterns, url
-from portal.plugins.gnmawsgr import views
+from django.core.urlresolvers import reverse_lazy
+import views
 from django.views.generic.simple import redirect_to
 
 urlpatterns = patterns(
     'portal.plugins.gnmawsgr.views',
-    url(r'^$', views.index),
+    url(r'^$', views.index, name="index"),
     url(r'^r/$', views.r, name="request"),
     url(r'^$', redirect_to, {'url': '/' }),
-    url(r'^status', views.CurrentStatusView.as_view(), name="status")
+    url(r'^status$', views.CurrentStatusView.as_view(), name="status"),
+    url(r'^status/$', redirect_to, {'url': reverse_lazy('status')}),
+    url(r'^status/(?P<pk>.*)/delete$', views.DeleteRestoreRequest.as_view(), name="delete_request"),
 )

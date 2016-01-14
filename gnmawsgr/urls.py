b7b2b0ffa31831ce5904d06b429c9f020b149bd2
david_allison <david.allison@theguardian.com>
2016-01-14 17:21:31 +0000

    Added various code to try to get around problems when restarting a job.

--- gnmawsgr/urls.py
+++ gnmawsgr/urls.py
@@ -7,6 +7,7 @@ urlpatterns = patterns(
     'portal.plugins.gnmawsgr.views',
     url(r'^$', views.index, name="index"),
     url(r'^r/$', views.r, name="request"),
+    url(r'^ra/$', views.ra, name="request"),
     url(r'^$', redirect_to, {'url': '/' }),
     url(r'^status$', views.CurrentStatusView.as_view(), name="status"),
     url(r'^status/$', redirect_to, {'url': reverse_lazy('status')}),

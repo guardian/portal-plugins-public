bfd89452ac382e88d583dd398e1cbf24859842cc
Dave Allison <david.allison@theguardian.com>
2016-02-01 16:26:17 +0000

    Added code which adds a metadata option.

--- gnmawsgr/management/commands/install_glacierrestore.py
+++ gnmawsgr/management/commands/install_glacierrestore.py
@@ -1,9 +1,10 @@
 __author__ = 'Andy Gallagher <andy.gallagher@theguardian.com>'
 
 from django.core.management.base import BaseCommand, CommandError
+from portal.plugins.gnmawsgr.vsmixin import VSMixin
 
 
-class Command(BaseCommand):
+class Command(VSMixin, BaseCommand):
     help = 'Installs the FCS S3-Glacier Restore plugin'
 
     def add_arguments(self, parser):
@@ -14,6 +15,53 @@ class Command(BaseCommand):
 
 ### END GNM AWS GR installation config
 """)
+
+    def _field_extra_data_node(self, doc):
+        for dataNode in doc.findall('{0}data'.format(self._ns)):
+            keyNode = dataNode.find('{0}key'.format(self._ns))
+            if keyNode.text == 'extradata':
+                valNode = dataNode.find('{0}value'.format(self._ns))
+                return valNode
+        return None
+
+    def _field_extra_data(self, doc):
+        import json
+        n = self._field_extra_data_node(doc)
+        return json.loads(n.text)
+
+    def _update_extra_data(self,doc,newval):
+        import json
+        n = self._field_extra_data_node(doc)
+        n.text = json.dumps(newval)
+
+    def add_choices_to_field(self, fieldname, choices):
+        import httplib2
+        import xml.etree.cElementTree as ET
+        from pprint import pprint
+
+        h=httplib2.Http()
+        headers, content = self._make_vidispine_request(h,"GET","/API/metadata-field/{0}?data=all".format(fieldname),
+                                                        body=None,headers={'Accept': 'application/xml'})
+        rootEl = ET.fromstring(content)
+        extra_data = self._field_extra_data(rootEl)
+
+        #pprint(extra_data)
+        if not isinstance(choices,list):
+            choices = [choices]
+
+        for c in choices:
+            if isinstance(c,tuple):
+                extra_data['values'].append({u'key': unicode(c[0]), u'value': unicode(c[1])})
+            else:
+                extra_data['values'].append({u'key': unicode(c), u'value': unicode(c)})
+        #pprint(extra_data)
+
+        self._update_extra_data(rootEl,extra_data)
+        #print ET.tostring(rootEl)
+
+        headers, content = self._make_vidispine_request(h,"PUT","/API/metadata-field/{0}".format(fieldname),
+                                                        body=ET.tostring(rootEl),headers={})
+
 
     def make_group(self, groupname, groupdesc=None):
         from xml.etree.ElementTree import Element,SubElement,tostring
@@ -52,6 +100,10 @@ class Command(BaseCommand):
         import os
         import sys
         import re
+
+        print "Ensuring that asset status values are present"
+        self.add_choices_to_field('gnm_asset_status', ['Waiting for Archive Restore'])
+        print "Done"
 
         print "Installing glacier restore role..."
         self.make_group('AWS_GR_Restore',"Add users to this group to allow them to use the S3-Glacier restore plugin")

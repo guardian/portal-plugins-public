0f11124b820da3e60406614515ae806f1b97a67c
Dave Allison <david.allison@theguardian.com>
2016-02-12 18:39:03 +0000

    Removing pointless code and commenting out some lines.

--- gnmpropagation/urls.py
+++ gnmpropagation/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls.defaults import patterns, url
-from django.core.urlresolvers import reverse_lazy
 import views
-from django.views.generic.simple import redirect_to
+#from django.views.generic.simple import redirect_to
 
 urlpatterns = patterns(
     'portal.plugins.gnmpropagation.views',

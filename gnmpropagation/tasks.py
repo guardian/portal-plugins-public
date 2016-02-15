ee5ea6dc2c3635a0b74493631a37b53a3867c41c
Andy Gallagher <andy.gallagher@theguardian.com>
2016-02-15 11:48:11 +0000

    tidyup

--- gnmpropagation/tasks.py
+++ gnmpropagation/tasks.py
@@ -1,20 +1,21 @@
 import celery
 import logging
 log = logging.getLogger(__name__)
-#log.info('Testing logging outside Celery task')
 
 try:
     logger = celery.utils.log.get_task_logger(__name__)
 except AttributeError:
     logger = logging.getLogger('main')
+    #do_task = propagate.delay(collectionid,field,switch)
 
-def do_propagate(collectionid,field,switch):
+rule_value_table = {
+    'gnm_storage_rule_sensitive': 'storage_rule_sensitive',
+    'gnm_storage_rule_deletable': 'storage_rule_deletable',
+    'gnm_storage_rule_deep_archive': 'storage_rule_deep_archive'
+}
 
-    #from pprint import pprint
-
-    do_task = do_propagate.delay(collectionid,field,switch)
-
-    #pprint(do_task.__dict__)
+class UnknownFieldError(StandardError):
+    pass
 
 @celery.task
 def propagate(collectionid,field,switch):
@@ -34,24 +35,28 @@ def propagate(collectionid,field,switch):
     #print field
     #print switch
 
-    VIDISPINE_URL = "http://127.0.0.1"
-    VIDISPINE_USERNAME = "admin"
-    VIDISPINE_PORT = 8080
-    VIDISPINE_PASSWORD = "admin"
+    #setswitch = None
 
-    setswitch = None
+    # if (field == 'gnm_storage_rule_sensitive') and (switch == '1'):
+    #     setswitch = 'storage_rule_sensitive'
+    #
+    # if (field == 'gnm_storage_rule_deletable') and (switch == '1'):
+    #     setswitch = 'storage_rule_deletable'
+    #
+    # if (field == 'gnm_storage_rule_deep_archive') and (switch == '1'):
+    #     setswitch = 'storage_rule_deep_archive'
 
-    if (field == 'gnm_storage_rule_sensitive') and (switch == '1'):
-        setswitch = 'storage_rule_sensitive'
-
-    if (field == 'gnm_storage_rule_deletable') and (switch == '1'):
-        setswitch = 'storage_rule_deletable'
-
-    if (field == 'gnm_storage_rule_deep_archive') and (switch == '1'):
-        setswitch = 'storage_rule_deep_archive'
-
-    if (switch != '1') and (switch != '0'):
-        setswitch = switch
+    try:
+        if switch:
+            setswitch = rule_value_table[field]
+        else:
+            setswitch = ""
+    except KeyError:
+        logger.error("I can't propagate the value of {0} as I do not have the string value to set it to.  Check rule_value_table in gnmproparation:tasks.py")
+        raise UnknownFieldError(field)
+    #
+    # if (switch != '1') and (switch != '0'):
+    #     setswitch = switch
 
     from django.conf import settings
 
@@ -59,8 +64,14 @@ def propagate(collectionid,field,switch):
     #collection_obj = VSCollection(url=VIDISPINE_URL,port=VIDISPINE_PORT,user=VIDISPINE_USERNAME,passwd=VIDISPINE_PASSWORD)
     collection_obj.populate(collectionid, specificFields=['title','gnm_asset_category'])
 
+    n=0
     for subitem in collection_obj.content(shouldPopulate=False):
+        logger.debug("propagating value from parent {0} to child {1} ({2})".format(collectionid,subitem.name,subitem.__class__))
+
         subitem.populate(subitem.name, specificFields=['title','gnm_asset_category','gnm_type',field])
+        logger.debug(u"title: {0}, category: {1}, type: {2}".format(subitem.get('title'),subitem.get('gnm_asset_category'),
+                                                                    subitem.get('gnm_type')))
+
         #pprint(subitem.__dict__)
         type = '(unknown)'
         if isinstance(subitem,VSItem):
@@ -75,6 +86,10 @@ def propagate(collectionid,field,switch):
 
         if subitem.get(field) != setswitch:
             subitem.set_metadata({field: setswitch})
+        logger.info("value set on {0}".format(subitem.name))
+        n+=1
+
+    logger.info("Propagation run completed for {0}.  Affected {1} items".format(collectionid, n))
 
 if __name__ == '__main__':
     import sys
@@ -82,5 +97,6 @@ if __name__ == '__main__':
     for arg in sys.argv:
         print arg
 
-    do_propagate(sys.argv[1],sys.argv[2],sys.argv[3])
+    propagate(sys.argv[1],sys.argv[2],sys.argv[3])
+    #do_propagate(sys.argv[1],sys.argv[2],sys.argv[3])
 

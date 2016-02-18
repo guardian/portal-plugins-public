22b74fb67b21495b146f99a8971d5fc10bd45b55
Andy Gallagher <andy.gallagher@theguardian.com>
2016-02-18 18:05:52 +0000

    Loads of nice endpoints that can send data to the Cocoa app

--- gnmextprojecthelper/urls.py
+++ gnmextprojecthelper/urls.py
@@ -4,7 +4,8 @@
 import logging
 
 from django.conf.urls.defaults import *
-from views import ProjectTemplateFileView,ProjectDefaultInformationView
+from views import ProjectTemplateFileView,ProjectDefaultInformationView,ProjectListView,CommissionListView,\
+    WorkingGroupListView, CommissionerListView, ProjectTypeListView,ProjectSubTypeListView
 # This new app handles the request to the URL by responding with the view which is loaded 
 # from portal.plugins.gnmextprojecthelper.views.py. Inside that file is a class which responsedxs to the 
 # request, and sends in the arguments template - the html file to view.
@@ -13,5 +14,11 @@ from views import ProjectTemplateFileView,ProjectDefaultInformationView
 urlpatterns = patterns('portal.plugins.gnmextprojecthelper.views',
     url(r'^$', 'GenericAppView', kwargs={'template': 'gnmextprojecthelper/index.html'}, name='index'),
     url(r'^get_template/(?P<project_type>[A-Za-z0-9/]+)', ProjectTemplateFileView.as_view(), name='project_template_download'),
-    url(r'^default_info/(?P<parent_commission>\w{2}-\d+)/*$', ProjectDefaultInformationView, name='new_project_defaults')
+    url(r'^default_info/(?P<parent_commission>\w{2}-\d+)/*$', ProjectDefaultInformationView, name='new_project_defaults'),
+    url(r'^project_list/(?P<commission_id>\w{2}-\d+)/*$', ProjectListView.as_view(), name='projects_for_commission'),
+    url(r'^commission_list/(?P<working_group>[\w\d]{8}-[\w\d]{4}-[\w\d]{4}-[\w\d]{4}-[\w\d]+)/*$',CommissionListView.as_view(), name='commissions_for_group'),
+    url(r'^working_group_list/*$', WorkingGroupListView.as_view(), name='working_groups'),
+    url(r'^commissioner_list/*$', CommissionerListView.as_view(), name='commissioners'),
+    url(r'^project_type_list/*$', ProjectTypeListView.as_view(), name='project_types'),
+    url(r'^project_subtype_list/(?P<project_type>[\w\d]+)/*$', ProjectSubTypeListView.as_view(), name='project_types'),
 )
--- gnmextprojecthelper/views.py
+++ gnmextprojecthelper/views.py
@@ -14,6 +14,8 @@ from portal.vidispine.iitem import ItemHelper
 from portal.vidispine.iexception import NotFoundError
 from django.views.generic import View
 from portal.plugins.gnm_projects.views import ProjectNewView
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 log = logging.getLogger(__name__)
 
@@ -111,3 +113,184 @@ class ProjectDefaultInformationView(ProjectNewView):
             return HttpResponse(json.dumps({'not_found': parent_commission}),status=404)
 
         return HttpResponse(json.dumps(self._initial_data(commission,request.user)))
+
+
+class CachedViewMixin(object):
+    def __init__(self,*args,**kwargs):
+        import memcache
+        from django.conf import settings
+        super(CachedViewMixin,self).__init__(*args,**kwargs)
+
+        self.mc = memcache.Client([settings.CACHE_LOCATION])
+
+
+class GenericGroupListView(CachedViewMixin, APIView):
+    from rest_framework.parsers import JSONParser
+    from rest_framework.renderers import JSONRenderer
+
+    parser_classes = (JSONParser, )
+    renderer_classes = (JSONRenderer, )
+
+    group_name = ""
+    data_desc = "pluto:none"
+    expiry_time=21600 #default: 6 hours
+
+    def get_data(self,**kwargs):
+        from vidispine.vs_globalmetadata import VSGlobalMetadata
+        from django.conf import settings
+        md = VSGlobalMetadata(url=settings.VIDISPINE_URL,port=settings.VIDISPINE_PORT,
+                              user=settings.VIDISPINE_USERNAME,passwd=settings.VIDISPINE_PASSWORD)
+
+        md.populate()
+        grp = md.get_group(self.group_name)
+        return grp.values()
+
+    def get(self,request,**kwargs):
+        data = None
+        if self.data_desc is not None:
+            data = self.mc.get(self.data_desc)
+        if data is None:
+            data = self.get_data(**kwargs)
+            if self.data_desc is not None:
+                self.mc.set(self.data_desc,data,time=self.expiry_time)
+        return Response({'status': 'ok', 'group_count': len(data), 'group_list': data})
+
+
+class WorkingGroupListView(GenericGroupListView):
+    group_name = "WorkingGroup"
+    data_desc = "pluto:working_group_list_cache"
+
+
+class CommissionerListView(GenericGroupListView):
+    group_name = "Commissioner"
+    data_desc = "pluto:commissioner_list_cache"
+
+
+class ProjectTypeListView(GenericGroupListView):
+    group_name = "ProjectType"
+    data_desc = "pluto:project_types_list"
+
+
+class ProjectSubTypeListView(GenericGroupListView):
+    group_name = "ProjectSubType"
+    data_desc = "pluto:project_sub_types:"
+    #data_desc = None
+
+    def get_data(self,**kwargs):
+        #kwargs are passed through from the request
+        from vidispine.vs_globalmetadata import VSGlobalMetadata
+        from django.conf import settings
+        from pprint import pprint
+        md = VSGlobalMetadata(url=settings.VIDISPINE_URL,port=settings.VIDISPINE_PORT,
+                              user=settings.VIDISPINE_USERNAME,passwd=settings.VIDISPINE_PASSWORD)
+
+        md.populate()
+        grp = md.get_group(self.group_name)
+        #pprint(grp.__dict__)
+        rtn = []
+        for v in grp.values():
+            if v['gnm_projectsubtype_projecttype'] == kwargs['project_type']:
+                rtn.append(v)
+
+        return rtn
+
+    def get(self,request,**kwargs):
+        self.data_desc = "pluto:project_sub_types:{0}".format(kwargs['project_type'])
+        return super(ProjectSubTypeListView,self).get(request,**kwargs)
+
+class GenericListView(APIView):
+    from rest_framework.parsers import JSONParser
+    from rest_framework.renderers import JSONRenderer
+
+    parser_classes = (JSONParser, )
+    renderer_classes = (JSONRenderer, )
+
+    interesting_fields = [
+        'title',
+        'gnm_project_type',
+        'gnm_project_headline',
+        'gnm_project_status',
+        'gnm_type'
+    ]
+
+
+    def get(self, request, search_criteria={}, response_extra={},*args,**kwargs):
+        from vidispine.vs_search import VSSearch
+        from django.conf import settings
+        from django.http import HttpResponseBadRequest
+        import traceback
+        import re
+
+        try:
+            s = VSSearch(url=settings.VIDISPINE_URL,user=settings.VIDISPINE_USERNAME,port=settings.VIDISPINE_PORT,
+                                   passwd=settings.VIDISPINE_PASSWORD, searchType="collection")
+            s.debug = False
+            s.addCriterion(search_criteria)
+
+            result = s.execute()
+            # if result.total_hits == 0:
+            #     return Response({'status': 'ok', 'projects': 0})
+
+            project_list = []
+            for item in result.results(shouldPopulate=False):
+                item.populate(item.name,specificFields=self.interesting_fields)
+                rec = {'itemId': item.name}
+                for f in self.interesting_fields:
+                    rec[f] = item.get(f)
+                project_list.append(rec)
+
+            rsp = {'status': 'ok', 'count': len(project_list), 'list': project_list}
+            rsp.update(response_extra)
+            return Response(rsp)
+        except Exception as e:
+            if settings.DEBUG:
+                return Response({'status': 'error', 'exception': str(e), 'trace': traceback.format_exc()},status=500)
+            else:
+                return Response({'status': 'error'},status=500)
+
+
+class ProjectListView(GenericListView):
+    interesting_fields = [
+        'title',
+        'gnm_project_type',
+        'gnm_project_headline',
+        'gnm_project_status',
+        'gnm_type'
+    ]
+
+    def get(self, request, **kwargs):
+        import re
+        from django.http import HttpResponseBadRequest
+
+        if not 'commission_id' in kwargs:
+            return HttpResponseBadRequest()
+        commission_id = kwargs['commission_id']
+
+        if not re.match(r'\w{2}-\d+',commission_id):
+            return HttpResponseBadRequest()
+
+        criteria = {'__parent_collection': commission_id}
+        return super(ProjectListView,self).get(request, search_criteria=criteria,response_extra={'commission': commission_id},
+                                                  **kwargs)
+
+
+class CommissionListView(GenericListView):
+    interesting_fields = [
+        'title',
+        'gnm_commission_headline',
+        'gnm_commission_status',
+        'gnm_commission_workinggroup',
+        'gnm_type'
+    ]
+
+    def get(self, request, *args,**kwargs):
+        from django.http import HttpResponseBadRequest
+        from pprint import pprint
+
+        if not 'working_group' in kwargs:
+            return HttpResponseBadRequest()
+
+        criteria = {'gnm_commission_workinggroup': kwargs['working_group'],
+                    'gnm_type': 'Commission'}
+        pprint(criteria)
+        return super(CommissionListView,self).get(request,search_criteria=criteria,**kwargs)

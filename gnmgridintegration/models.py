27695c5c2490c894037c668adb1073d5f6b3add0
Andy Gallagher <andy.gallagher@theguardian.com>
2016-02-27 14:06:03 +0000

    Adding in customisable metadata output

--- gnmgridintegration/models.py
+++ gnmgridintegration/models.py
@@ -1,17 +1,25 @@
 from django.db import models
+from django.utils import html
 import logging
 
 logger = logging.getLogger(__name__)
 
 
 class GridMetadataFields(models.Model):
-    grid_field_name = models.CharField(max_length=255)
-    format_string = models.CharField(max_length=4096)
-    vs_field = models.CharField(max_length=255, blank=True)
-    type = models.IntegerField(choices=((1, 'ItemMetadata'),(2, 'RightsMetadata')))
+    grid_field_name = models.CharField(max_length=255, help_text=html.escape('The name of the metadata field to set in The Grid'))
+    format_string = models.CharField(max_length=4096,
+                                     help_text=html.escape('''The value to set.  {vs_field_value} and {frame_number} will be substituted.''')
+                                     )
+    vs_field = models.CharField(max_length=255, blank=True,
+                                help_text=html.escape('Read the value of this metadata field and substitute it in to format_string as {vs_field_value}'))
+    type = models.IntegerField(choices=((1, 'ItemMetadata'),(2, 'RightsMetadata')),
+                               help_text=html.escape('Should the field be set as item metadata or rights metadata?'))
 
     def __unicode__(self):
-        return u'Set {0} from {1}'.format(self.grid_field_name, self.vs_field)
+        if self.vs_field is not None and self.vs_field != "":
+            return u'Set Grid field "{0}" from {1}'.format(self.grid_field_name, self.vs_field)
+        else:
+            return u'Set Grid field "{0}" from static string'.format(self.grid_field_name)
 
     def real_value(self, vsitem, **kwargs):
         """
@@ -36,5 +44,5 @@ class GridMetadataFields(models.Model):
         return self.format_string.format(format_params)
 
     class Meta:
-        ordering = ['grid_field_name']
+        ordering = ['type','grid_field_name']
 

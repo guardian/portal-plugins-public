6530e238ab2804a3fa6687f23500cf577e09d32f
Andy Gallagher <andy.gallagher@theguardian.com>
2016-03-04 21:41:00 +0000

    bugfixes

--- gnmextprojecthelper/management/commands/uninstall_projecthelper.py
+++ gnmextprojecthelper/management/commands/uninstall_projecthelper.py
@@ -1,8 +1,8 @@
 from django.core.management.base import BaseCommand, CommandError
-from portal.plugins.gnmgridintegration.management.management_mixin import ManagementMixin
+from portal.plugins.gnmextprojecthelper.management.management_mixin import ManagementMixin
 
 class Command(ManagementMixin, BaseCommand):
-    help = 'Removes the Grid integrator plugin'
+    help = 'Removes the GNM external project helper plugin'
 
     def handle(self, *args, **options):
         self.update_config_file({}, remove=True)

8625bd8a8880d320418905e007b54917e73bb7c8
Andy Gallagher <andy.gallagher@theguardian.com>
2016-05-04 19:03:51 +0100

    initial version of a plugin to profile time taken to master videos. Data extraction is working, now need to present it.

---
+++ gnmuploadprofiler/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.utils import html
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class OutputTimings(models.Model):
+    item_id = models.CharField(max_length=255,unique=True)
+    item_duration = models.FloatField(default=0.0)
+    created_time = models.DateTimeField()
+    proxy_completed_interval = models.FloatField(blank=True,default=0.0)
+    upload_trigger_interval = models.FloatField(blank=True,default=0.0)
+    page_created_interval = models.FloatField(blank=True,default=0.0)
+    final_transcode_completed_interval = models.FloatField(blank=True,default=0.0)
+    page_launch_guess_interval = models.FloatField(blank=True,default=0.0)
+    completed_time = models.DateTimeField(blank=True)

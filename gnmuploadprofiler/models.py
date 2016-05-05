3ce52ccc15c090f6dc6a6772608492df076eb205
Andy Gallagher <andy.gallagher@theguardian.com>
2016-05-05 16:18:27 +0100

    work on data aggregation and display

--- gnmuploadprofiler/models.py
+++ gnmuploadprofiler/models.py
@@ -6,7 +6,7 @@ logger = logging.getLogger(__name__)
 
 
 class OutputTimings(models.Model):
-    item_id = models.CharField(max_length=255,unique=True)
+    item_id = models.CharField(max_length=255,unique=True,db_index=True)
     item_duration = models.FloatField(default=0.0)
     created_time = models.DateTimeField()
     proxy_completed_interval = models.FloatField(blank=True,default=0.0)
@@ -15,3 +15,6 @@ class OutputTimings(models.Model):
     final_transcode_completed_interval = models.FloatField(blank=True,default=0.0)
     page_launch_guess_interval = models.FloatField(blank=True,default=0.0)
     completed_time = models.DateTimeField(blank=True)
+
+    class Meta:
+        ordering = ['-completed_time','-created_time']
--- gnmuploadprofiler/urls.py
+++ gnmuploadprofiler/urls.py
@@ -4,7 +4,7 @@
 import logging
 
 from django.conf.urls.defaults import *
-
+from views import BasicDataView
 # This new app handles the request to the URL by responding with the view which is loaded 
 # from portal.plugins.gnmuploadprofiler.views.py. Inside that file is a class which responsedxs to the 
 # request, and sends in the arguments template - the html file to view.
@@ -12,4 +12,6 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('portal.plugins.gnmuploadprofiler.views',
     url(r'^$', 'GenericAppView', kwargs={'template': 'gnmuploadprofiler/index.html'}, name='index'),
+    url(r'^test/(\d{4}\d{2}\d{2})$', BasicDataView.as_view(), name='basic_data_specific'),
+    url(r'^test/$', BasicDataView.as_view(), name='basic_data')
 )

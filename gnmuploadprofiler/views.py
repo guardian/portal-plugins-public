fe986912c90c472fc5e6491d63943d5e3246ad50
Andy Gallagher <andy.gallagher@theguardian.com>
2016-05-10 12:30:22 +0100

    allowing recalculation of averages when filtering, and javascript injection

--- gnmuploadprofiler/views.py
+++ gnmuploadprofiler/views.py
@@ -11,7 +11,9 @@ import logging
 from django.contrib.auth.decorators import login_required
 from django.views.generic import TemplateView, ListView
 from django.http import HttpResponseBadRequest
-
+from rest_framework.renderers import JSONRenderer
+from rest_framework.authentication import SessionAuthentication
+from rest_framework.permissions import IsAuthenticated
 from portal.generic.baseviews import ClassView
 from portal.vidispine.iitem import ItemHelper
 from portal.vidispine.iexception import NotFoundError
@@ -41,62 +43,133 @@ class GenericAppView(ClassView):
 GenericAppView = GenericAppView._decorate(login_required)
 
 
-class BasicDataView(ListView):
+class DataMixin(object):
+    """
+    Mixin to abstract the shared data parts of display views
+    """
+    max_items = 100
+
+    interesting_fields = [
+        'item_duration',
+        'proxy_completed_interval',
+        'upload_trigger_interval',
+        'page_created_interval',
+        'final_transcode_completed_interval',
+        'page_launch_guess_interval',
+        'page_launch_capi_interval'
+    ]
+
+    @staticmethod
+    def median_value(queryset, term):
+        count = queryset.count()
+        values = queryset.values_list(term, flat=True).order_by(term)
+        if count % 2 == 1:
+            return values[int(round(count / 2))]
+        else:
+            return sum(values[count / 2:count / 2 + 2]) / 2.0
+
+    def calculate_averages(self,qs):
+        from django.db.models import Avg, StdDev, Variance, Sum, Min, Max
+        averages = {}
+
+        for x in self.interesting_fields:
+            # for x in OutputTimings._meta.get_all_field_names():
+            data = qs.aggregate(Avg(x), StdDev(x), Min(x), Max(x))
+
+            stkeyname = "{0}__stddev".format(x)
+            avkeyname = "{0}__avg".format(x)
+
+            data[stkeyname] = "{0:.1f}%".format((data[stkeyname] / data[avkeyname]) * 100)
+
+            averages.update(data)
+        return averages
+
+
+class AveragesRESTView(DataMixin, APIView):
+    authentication_classes = (SessionAuthentication, )
+    permission_classes = (IsAuthenticated, )
+    renderer_classes = (JSONRenderer,)
+
+    def get_queryset(self):
+        qs = OutputTimings.objects.all()
+
+        if 'date' in self.request.GET:
+            parts = re.match(r'(\d{4})(\d{2})(\d{2})', self.request.GET['date'])
+            starting_time = datetime(year=int(parts.group(1)),
+                                     month=int(parts.group(2)),
+                                     day=int(parts.group(3)), )
+            ending_time = datetime(year=int(parts.group(1)),
+                                   month=int(parts.group(2)),
+                                   day=int(parts.group(3)),
+                                   hour=23, minute=59, second=59, microsecond=999999)
+
+            qs = qs.filter(completed_time__gte=starting_time, completed_time__lt=ending_time)
+        if 'commission' in self.request.GET:
+            qs = qs.filter(commission=self.request.GET['commission'])
+        if 'project' in self.request.GET:
+            qs = qs.filter(project=self.request.GET['project'])
+        if 'sort' in self.request.GET:
+            qs = qs.order_by(self.request.GET['sort'])
+
+        return qs
+
+    def get(self,request):
+        from templatetags.uploadprofiler_customfilters import time_display
+        qs = self.get_queryset()
+        data = self.calculate_averages(qs)
+
+        #if 'median' in self.request.GET:
+
+        for fieldname in self.interesting_fields:
+            data[fieldname + "__median"] = self.median_value(qs,fieldname)
+
+        for k,v in data.items():
+            if isinstance(v,float) or isinstance(v,int):
+                keyname = k #+ "_formatted"
+                data[keyname] = time_display(v)
+
+        return Response(data)
+
+
+class BasicDataView(DataMixin, ListView):
     model = OutputTimings
     template_name = "gnmuploadprofiler/simple_data.html"
 
     def get_queryset(self):
-        qs = super(BasicDataView,self).get_queryset()
+        qs = super(BasicDataView, self).get_queryset()
 
         if 'date' in self.request.GET:
-            parts = re.match(r'(\d{4})(\d{2})(\d{2})',self.request.GET['date'])
+            parts = re.match(r'(\d{4})(\d{2})(\d{2})', self.request.GET['date'])
             starting_time = datetime(year=int(parts.group(1)),
                                      month=int(parts.group(2)),
-                                     day=int(parts.group(3)),)
+                                     day=int(parts.group(3)), )
             ending_time = datetime(year=int(parts.group(1)),
-                                     month=int(parts.group(2)),
-                                     day=int(parts.group(3)),
-                                   hour=23,minute=59,second=59,microsecond=999999)
+                                   month=int(parts.group(2)),
+                                   day=int(parts.group(3)),
+                                   hour=23, minute=59, second=59, microsecond=999999)
 
-            qs = qs.filter(completed_time__gte=starting_time,completed_time__lt=ending_time)
+            qs = qs.filter(completed_time__gte=starting_time, completed_time__lt=ending_time)
         if 'sort' in self.request.GET:
             qs = qs.order_by(self.request.GET['sort'])
 
         return qs
 
     def get_context_data(self, **kwargs):
-        from django.db.models import Avg, StdDev, Variance,Sum, Min, Max
-
         qs = kwargs['object_list']
-        averages = {}
-
-        for x in ['item_duration','proxy_completed_interval','upload_trigger_interval',
-                  'page_created_interval','final_transcode_completed_interval','page_launch_guess_interval',
-                  'page_launch_capi_interval']:
-        #for x in OutputTimings._meta.get_all_field_names():
-            data = qs.aggregate(Avg(x),StdDev(x),Min(x),Max(x))
-
-            stkeyname = "{0}__stddev".format(x)
-            avkeyname = "{0}__avg".format(x)
-
-            data[stkeyname] = "{0:.1f}%".format((data[stkeyname]/data[avkeyname])*100)
-
-            averages.update(data)
-
         ctx = super(BasicDataView, self).get_context_data(**kwargs)
         #ctx['averages'] = qs.aggregate(Avg('item_duration'),StdDev('item_duration')))
-        ctx['averages'] = averages
+        ctx['averages'] = self.calculate_averages(qs[:self.max_items])
         return ctx
 
 
 class ItemInfoAPIView(APIView):
-    from rest_framework.parsers import JSONParser, XMLParser
-    from rest_framework.renderers import JSONRenderer, XMLRenderer
+    from rest_framework.parsers import JSONParser
+    from rest_framework.renderers import JSONRenderer
     from rest_framework import permissions
 
     permission_classes = (permissions.IsAuthenticated,)
-    parser_classes = (JSONParser,XMLParser,)
-    renderer_classes = (JSONRenderer,XMLRenderer,)
+    parser_classes = (JSONParser,)
+    renderer_classes = (JSONRenderer,)
 
     interesting_fields = [
         'title',
@@ -111,8 +184,9 @@ class ItemInfoAPIView(APIView):
         from django.core import cache
         from django.conf import settings
 
-        if not re.match(r'\w{2}-\d+$',itemid):
-            return HttpResponseBadRequest("Item ID invalid")
+        #not necessary, as this is already done by the url parser
+        # if not re.match(r'\w{2}-\d+$',itemid):
+        #     return HttpResponseBadRequest("Item ID invalid")
 
         c = cache.get_cache('default')
 

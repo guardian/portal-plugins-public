c334218b26b8acbf746649740e9a9267ee522f93
Andy Gallagher <andy.gallagher@theguardian.com>
2016-12-05 13:57:29 +0000

    putting in circle build stuff, updating buildrpms to work better with circle

---
+++ gnmpropagation/testsetup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#this script is run from circle.yml when setting up for a CI build
+BL_DEPLOY_PATH=/opt/cantemo/portal/portal/plugins/gnm_businesslogic/logic
+sudo mkdir -p ${BL_DEPLOY_PATH}
+sudo chown ubuntu ${BL_DEPLOY_PATH}
+
+for x in `ls gnmpropagation/businesslogic/*.xml`; do
+    ln -s $x "${BL_DEPLOY_PATH}"
+done

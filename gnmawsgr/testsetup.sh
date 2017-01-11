6448ff6ce5ed4bdc84e86a1369ef159186af1995
Dave Allison <david.allison@theguardian.com>
2017-01-11 13:07:00 +0000

    Adding the required Portal theme files for the RPM to build and a script to copy them to the required paths.

---
+++ gnmawsgr/testsetup.sh
@@ -0,0 +1,10 @@
+mkdir -p /opt/cantemo/portal/portal_themes/gnm/templates/media
+cp -a portal_theme_files/media_view.html /opt/cantemo/portal/portal_themes/gnm/templates/media/media_view.html
+cp -a portal_theme_files/media_inc_preview.html /opt/cantemo/portal/portal_themes/gnm/templates/media/media_inc_preview.html
+mkdir -p /opt/cantemo/portal/portal_media/gnm/css
+cp -a portal_theme_files/logos.css /opt/cantemo/portal/portal_media/gnm/css/logos.css
+mkdir -p /opt/cantemo/portal/portal_media/img/gnm/logos
+cp -a portal_theme_files/axe.gif /opt/cantemo/portal/portal_media/img/gnm/logos/axe.gif
+cp -a portal_theme_files/archived.png /opt/cantemo/portal/portal_media/img/gnm/logos/archived.png
+cp -a portal_theme_files/restored.png /opt/cantemo/portal/portal_media/img/gnm/logos/restored.png
+cp -a portal_theme_files/delete.png /opt/cantemo/portal/portal_media/delete.png

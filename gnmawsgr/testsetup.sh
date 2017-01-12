6c9152df74f9b0aa76ab7e63558d171ef9807eb2
Dave Allison <david.allison@theguardian.com>
2017-01-12 11:54:04 +0000

    Attempting to fix path problems.

--- gnmawsgr/testsetup.sh
+++ gnmawsgr/testsetup.sh
@@ -3,21 +3,21 @@
 echo Attempting to copy Portal theme files to the required paths
 sudo mkdir -p /opt/cantemo/portal/portal_themes/gnm/templates/media
 sudo chown ubuntu /opt/cantemo/portal/portal_themes/gnm/templates/media
-sudo cp -a portal_theme_files/media_view.html /opt/cantemo/portal/portal_themes/gnm/templates/media/media_view.html
+sudo cp -a /home/ubuntu/portal-plugins-public/gnmawsgr/portal_theme_files/media_view.html /opt/cantemo/portal/portal_themes/gnm/templates/media/media_view.html
 sudo chown ubuntu /opt/cantemo/portal/portal_themes/gnm/templates/media/media_view.html
-sudo cp -a portal_theme_files/media_inc_preview.html /opt/cantemo/portal/portal_themes/gnm/templates/media/media_inc_preview.html
+sudo cp -a /home/ubuntu/portal-plugins-public/gnmawsgr/portal_theme_files/media_inc_preview.html /opt/cantemo/portal/portal_themes/gnm/templates/media/media_inc_preview.html
 sudo chown ubuntu /opt/cantemo/portal/portal_themes/gnm/templates/media/media_inc_preview.html
 sudo mkdir -p /opt/cantemo/portal/portal_media/gnm/css
 sudo chown ubuntu /opt/cantemo/portal/portal_media/gnm/css
-sudo cp -a portal_theme_files/logos.css /opt/cantemo/portal/portal_media/gnm/css/logos.css
+sudo cp -a /home/ubuntu/portal-plugins-public/gnmawsgr/portal_theme_files/logos.css /opt/cantemo/portal/portal_media/gnm/css/logos.css
 sudo chown ubuntu /opt/cantemo/portal/portal_media/gnm/css/logos.css
 sudo mkdir -p /opt/cantemo/portal/portal_media/img/gnm/logos
 sudo chown ubuntu /opt/cantemo/portal/portal_media/img/gnm/logos
-sudo cp -a portal_theme_files/axe.gif /opt/cantemo/portal/portal_media/img/gnm/logos/axe.gif
+sudo cp -a /home/ubuntu/portal-plugins-public/gnmawsgr/portal_theme_files/axe.gif /opt/cantemo/portal/portal_media/img/gnm/logos/axe.gif
 sudo chown ubuntu /opt/cantemo/portal/portal_media/img/gnm/logos/axe.gif
-sudo cp -a portal_theme_files/archived.png /opt/cantemo/portal/portal_media/img/gnm/logos/archived.png
+sudo cp -a /home/ubuntu/portal-plugins-public/gnmawsgr/portal_theme_files/archived.png /opt/cantemo/portal/portal_media/img/gnm/logos/archived.png
 sudo chown ubuntu /opt/cantemo/portal/portal_media/img/gnm/logos/archived.png
-sudo cp -a portal_theme_files/restored.png /opt/cantemo/portal/portal_media/img/gnm/logos/restored.png
+sudo cp -a /home/ubuntu/portal-plugins-public/gnmawsgr/portal_theme_files/restored.png /opt/cantemo/portal/portal_media/img/gnm/logos/restored.png
 sudo chown ubuntu /opt/cantemo/portal/portal_media/img/gnm/logos/restored.png
-sudo cp -a portal_theme_files/delete.png /opt/cantemo/portal/portal_media/delete.png
+sudo cp -a /home/ubuntu/portal-plugins-public/gnmawsgr/portal_theme_files/delete.png /opt/cantemo/portal/portal_media/delete.png
 sudo chown ubuntu /opt/cantemo/portal/portal_media/delete.png

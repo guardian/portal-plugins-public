2d8efe2748691050c4a52dd1cc4ed10e8aeb19c2
Dave Allison <david.allison@theguardian.com>
2017-03-29 17:10:14 +0100

    Tidying up various files

--- gnmpagerduty/tasks.py
+++ gnmpagerduty/tasks.py
@@ -1,4 +1,3 @@
-#import celery
 import logging
 from django.conf import settings
 from gnmvidispine.vs_storage import VSStoragePathMap
@@ -9,21 +8,12 @@ app = Celery()
 
 
 log = logging.getLogger(__name__)
-log.info('Testing logging outside Celery task. PagerDuty.')
-
-#try:
-#    logger = celery.utils.log.get_task_logger(__name__)
-#except AttributeError:
-#    logger = logging.getLogger('main')
 
 logger = logging.getLogger('main')
 
-#settings.PAGERDUTY_KEY
 
-#@celery.task
 @periodic_task(run_every=(crontab(minute='*/10')), name="check_storage", ignore_result=True)
 def check_storage():
-    from pprint import pprint
     from models import StorageData, IncidentKeys
     import requests
     import json
@@ -31,7 +21,6 @@ def check_storage():
     logger.info("check_storage run by Celery.")
 
     i = app.control.inspect()
-    print i.active()
 
     running = str(i.active())
 
@@ -50,10 +39,8 @@ def check_storage():
 
     url = 'https://events.pagerduty.com/generic/2010-04-15/create_event.json'
 
-    n = 0
-
     for val in sd['map']:
-        print(val['name'])
+
         try:
             record = StorageData.objects.get(storage_id=val['name'])
             try:
@@ -61,41 +48,23 @@ def check_storage():
             except Exception as e:
                 key = IncidentKeys.objects.create(storage_id=val['name'])
 
-            print(record.__dict__)
-            print record.trigger_size
-            print val['freeCapacity']
-            if key.incident_key != '':
-                print "Old key: ", key.incident_key
-
             intts = int(record.trigger_size)
             intfc = int(val['freeCapacity'])
 
             if intfc < intts:
-                print 'Running at point one'
+
                 returnthis = returnthis + " " + val['name']
                 if key.incident_key == '':
-                    print 'Running at point two'
+
                     payload = {
                         "service_key": settings.PAGERDUTY_KEY,
                         "event_type": "trigger",
                         "description": "Storage {0} lacks sufficient free space".format(val['contentDict']['name']),
-                        #"client": "GNM PagerDuty",
-                        #"client_url": "https://monitoring.service.com",
                         "details": {
                             "Vidispine ID": val['name'],
                             "Name": val['contentDict']['name'],
                             "Free Capacity": val['freeCapacity']
                             },
-                        #"contexts":[
-                        #    {
-                        #    "type": "link",
-                        #    "href": "http://acme.pagerduty.com"
-                        #    },{
-                        #    "type": "link",
-                        #    "href": "http://acme.pagerduty.com",
-                        #    "text": "View the incident on PagerDuty"
-                        #    }
-                        #]
                     }
 
                     r = requests.post(url, data=json.dumps(payload))
@@ -105,19 +74,14 @@ def check_storage():
                     key.incident_key = pddata['incident_key']
                     key.save()
 
-                    print r.text
-                    print r.status_code
             else:
-                print 'Running at point three'
-                if key.incident_key != '':
 
-                    print 'Running at point four'
+                if key.incident_key != '':
 
                     payload = {
                         "service_key": settings.PAGERDUTY_KEY,
                         "event_type": "resolve",
                         "description": "Storage {0} no longer lacks sufficient free space".format(val['contentDict']['name']),
-                        #"client": "GNM PagerDuty",
                         "incident_key": key.incident_key,
                         "details": {
                             "Vidispine ID": val['name'],
@@ -128,18 +92,13 @@ def check_storage():
 
                     r = requests.post(url, data=json.dumps(payload))
 
-                    print r.text
-                    print r.status_code
-
                     if r.status_code == 200:
-                        print 'Running at point five'
+
                         key.incident_key = ''
                         key.save()
 
         except Exception as e:
-            print 'try code went wrong'
-
-        n = (n + 1)
+            print 'An error occurred'
 
     if returnthis is "Under threshold:":
         returnthis = "No storages are under their thresholds."
--- gnmpagerduty/views.py
+++ gnmpagerduty/views.py
@@ -1,20 +1,17 @@
 import logging
 from portal.generic.baseviews import ClassView
-from django.contrib.auth.decorators import permission_required, login_required
-from django.views.generic import ListView, CreateView, DeleteView, UpdateView
-from django.core.urlresolvers import reverse_lazy
-from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
+from django.views.generic import ListView
 from models import StorageData
 from django.conf import settings
 from serializers import StorageDataSerializer
-from rest_framework.mixins import ListModelMixin, UpdateModelMixin
 from rest_framework.generics import ListAPIView, CreateAPIView, UpdateAPIView
 from rest_framework.renderers import JSONRenderer
 from rest_framework.parsers import JSONParser
 from rest_framework.response import Response
 import traceback
 from gnmvidispine.vs_storage import VSStoragePathMap
-import pprint as pprint
+
 log = logging.getLogger(__name__)
 
 class GenericAppView(ClassView):
@@ -41,9 +38,6 @@ class StorageDataList(ListAPIView):
 
 
 class StorageDataAdd(CreateAPIView):
-    def __init__(self):
-        print 'test'
-        log.info('test')
     serializer_class = StorageDataSerializer
     parser_classes = (JSONParser, )
     renderer_classes = (JSONRenderer, )
@@ -55,17 +49,7 @@ class StorageDataUpdate(UpdateAPIView):
     parser_classes = (JSONParser, )
     renderer_classes = (JSONRenderer, )
 
-    def __init__(self):
-        print 'test from StorageDataUpdate'
-        log.info('test from StorageDataUpdate')
-
     def put(self, *args, **kwargs):
-        #from pprint import pprint
-        #import traceback
-        #try:
-        #    super(StorageDataUpdate,self).put(*args,**kwargs)
-        #except Exception as e:
-        #    return Response({'status': 'error','exception': str(e), 'trace': traceback.format_exc()}, status=500)
 
         try:
             data = self.request.DATA
@@ -79,25 +63,20 @@ class StorageDataUpdate(UpdateAPIView):
         except Exception as e:
             return Response({'status': 'error','exception': str(e), 'trace': traceback.format_exc()}, status=500)
 
-        #except StorageData.DoesNotExist:
-        #    record = StorageData()
         return self.response
 
 
 class ConfigAlertsView(ListView):
-    #@method_decorator(permission_required('change_storage_alerts', login_url='/authentication/login', raise_exception=True))
-    #def dispatch(self, request, *args, **kwargs):
-    #    return super(ConfigAlertsView,self).dispatch(request,*args,**kwargs)
+
     model = StorageData
     template_name = "gnmpagerduty/admin_list.html"
 
     def get_context_data(self, modelready=model, **kwargs):
-        from pprint import pprint
+
         ctx = super(ConfigAlertsView, self).get_context_data(**kwargs)
         self.map = VSStoragePathMap(url=settings.VIDISPINE_URL,port=settings.VIDISPINE_PORT,
                                     user=settings.VIDISPINE_USERNAME,passwd=settings.VIDISPINE_PASSWORD,
                                     run_as=self.request.user.username)
-        pprint(self.map)
 
         ctx['map'] = map(lambda (k,v): v.__dict__, self.map.items())
 
@@ -114,38 +93,4 @@ class ConfigAlertsView(ListView):
 
             n = (n + 1)
 
-        #pprint(ctx['map'])
-        #pprint(ctx['map'][0]['name'])
-        #pprint(modelready.objects.values_list())
-        #dirty = modelready.objects.values_list('trigger_size', flat=True)
-        #try:
-        #    ctx['data'] = int(dirty[0])
-        #except Exception as e:
-        #    ctx['data'] = 0
-
         return ctx
-
-
-class AlertsEditView(UpdateView):
-    @method_decorator(permission_required('change_storage_alerts', login_url='/authentication/login', raise_exception=True))
-    def dispatch(self, request, *args, **kwargs):
-        return super(AlertsEditView,self).dispatch(request,*args,**kwargs)
-    model = StorageData
-    template_name = "gnmpagerduty/edit.html"
-    success_url = reverse_lazy('gnmpagerduty_admin')
-
-class AlertsDeleteView(DeleteView):
-    @method_decorator(permission_required('change_storage_alerts', login_url='/authentication/login', raise_exception=True))
-    def dispatch(self, request, *args, **kwargs):
-        return super(AlertsDeleteView,self).dispatch(request,*args,**kwargs)
-    model = StorageData
-    template_name = "gnmpagerduty/delete.html"
-    success_url = reverse_lazy('gnmpagerduty_admin')
-
-class AlertsCreateView(CreateView):
-    @method_decorator(permission_required('change_storage_alerts', login_url='/authentication/login', raise_exception=True))
-    def dispatch(self, request, *args, **kwargs):
-        return super(AlertsCreateView,self).dispatch(request,*args,**kwargs)
-    model = StorageData
-    template_name = "gnmpagerduty/edit.html"
-    success_url = reverse_lazy('gnmpagerduty_admin')

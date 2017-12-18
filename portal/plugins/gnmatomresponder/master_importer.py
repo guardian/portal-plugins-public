9361070d78e2afccab0ed0c0f001a16b47120856
Andy Gallagher <andy.gallagher@theguardian.com>
2017-12-18 17:27:25 +0000

    Also handling NotAProjectError

--- portal/plugins/gnmatomresponder/master_importer.py
+++ portal/plugins/gnmatomresponder/master_importer.py
@@ -31,6 +31,8 @@ class MasterImportResponder(KinesisResponder, S3Mixin, VSMixin):
         :param approx_arrival:
         :return:
         """
+        from .exceptions import NotAProjectError
+
         content = json.loads(record)
 
         logger.info(content)
@@ -48,11 +50,24 @@ class MasterImportResponder(KinesisResponder, S3Mixin, VSMixin):
             except UnicodeEncodeError:
                 pass
             project_collection = None
+        except NotAProjectError:
+            try:
+                logger.warning(u"Collection {0} specified for atom {1} ({2}) is not a Project. Falling back to default.".format(
+                    content['projectId'], content['atomId'],
+                    content.get('title','(unknown title)').encode("UTF-8","backslashescape")
+                ))
+            except UnicodeDecodeError:
+                pass
+            except UnicodeEncodeError:
+                pass
+            project_collection = None
 
         if project_collection is None:
-            project_collection = getattr(settings,'ATOM_RESPONDER_DEFAULT_PROJECTID',None)
-            if project_collection is None:
+            collection_id = getattr(settings,'ATOM_RESPONDER_DEFAULT_PROJECTID',None)
+            if collection_id is None:
                 raise RuntimeError("Unable to get a project ID for atom {0}, and no default is set".format(content['atomId']))
+            else:
+                project_collection = self.get_collection_for_id(collection_id)
 
         #We get two types of message on the stream, one for incoming xml the other for incoming media.
         if content['type'] == const.MESSAGE_TYPE_MEDIA:
@@ -64,7 +79,7 @@ class MasterImportResponder(KinesisResponder, S3Mixin, VSMixin):
                                                                  parent=project_collection
                                                                  )
                 logger.info("Created item {0} for atom {1}".format(master_item.name, content['atomId']))
-            return self.import_new_item(master_item, content)
+            return self.import_new_item(master_item, content, parent=project_collection)
         elif content['type'] == const.MESSAGE_TYPE_PAC:
             record = self.register_pac_xml(content)
             self.ingest_pac_xml(record)
@@ -135,9 +150,12 @@ class MasterImportResponder(KinesisResponder, S3Mixin, VSMixin):
                            s3_path=content['s3Key'])
         record.save()
 
-        logger.info(u"{0}: Adding item {1} to collection {2}".format(content['atomId'], master_item.name, parent.name))
-        parent.addToCollection(master_item)
-        logger.info(u"{0}: Done".format(content['atomId']))
+        if parent is not None:
+            logger.info(u"{0}: Adding item {1} to collection {2}".format(content['atomId'], master_item.name, parent.name))
+            parent.addToCollection(master_item)
+            logger.info(u"{0}: Done".format(content['atomId']))
+        else:
+            logger.error(u"{0}: No parent collection specified for item {1}!".format(content['atomId'], master_item.name))
         
     def ingest_pac_xml(self, pac_xml_record):
         """

37454f95bea1cf022b4d98adbdcfb1c8f768e390
Andy Gallagher <andy.gallagher@theguardian.com>
2018-04-16 10:11:51 +0100

    test fixes

--- portal/plugins/gnmoptin/tests/test_templates.py
+++ portal/plugins/gnmoptin/tests/test_templates.py
@@ -16,7 +16,7 @@ class TestTemplates(django.test.TestCase):
 
     def test_my_optins(self):
         rendered_content = self._dorender("my_optins.html",{})
-        self.assertIn('<li>No items yet.  Why not <a href=', rendered_content)
+        self.assertIn('<li>You have not enabled any optional features at the moment.  Why not <a href=', rendered_content)
 
     def test_add_optins(self):
         rendered_content = self._dorender("add_optins.html",{})
@@ -32,10 +32,9 @@ class TestTemplates(django.test.TestCase):
     def test_delete_optins(self):
         rendered_content = self._dorender("delete_optins.html",{})
         self.assertIn('<form action="" method="post">', rendered_content)
-        self.assertIn('<p>Are you sure you want to delete ""?</p>', rendered_content)
+        self.assertIn('<p>Are you sure you want to opt out of ""?</p>', rendered_content)
         self.assertIn('<input type="submit" value="Confirm" />', rendered_content)
 
     def test_user_profile_menu(self):
         rendered_content = self._dorender("user_profile_menu.html",{})
         self.assertIn('<span class="iicon" style="background-position:-197px -334px"></span>My Opt-Ins</a>', rendered_content)
-        self.assertIn('<span class="iicon" style="background-position:-197px -334px"></span>Add Opt-In</a>', rendered_content)

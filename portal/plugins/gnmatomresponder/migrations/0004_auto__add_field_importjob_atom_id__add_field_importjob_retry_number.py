6dec2ee819294373775ea859b41ea035344fae34
Andy Gallagher <andy.gallagher@theguardian.com>
2018-04-27 12:15:56 +0100

    adding missed migration

---
+++ portal/plugins/gnmatomresponder/migrations/0004_auto__add_field_importjob_atom_id__add_field_importjob_retry_number.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding field 'ImportJob.atom_id'
+        db.add_column('gnmatomresponder_importjob', 'atom_id',
+                      self.gf('django.db.models.fields.CharField')(default=-1, max_length=64, db_index=True),
+                      keep_default=False)
+
+        # Adding field 'ImportJob.retry_number'
+        db.add_column('gnmatomresponder_importjob', 'retry_number',
+                      self.gf('django.db.models.fields.IntegerField')(default=0),
+                      keep_default=False)
+
+
+    def backwards(self, orm):
+        # Deleting field 'ImportJob.atom_id'
+        db.delete_column('gnmatomresponder_importjob', 'atom_id')
+
+        # Deleting field 'ImportJob.retry_number'
+        db.delete_column('gnmatomresponder_importjob', 'retry_number')
+
+
+    models = {
+        'gnmatomresponder.importjob': {
+            'Meta': {'ordering': "['-started_at']", 'object_name': 'ImportJob'},
+            'atom_id': ('django.db.models.fields.CharField', [], {'max_length': '64', 'db_index': 'True'}),
+            'atom_title': ('django.db.models.fields.CharField', [], {'default': "'Unknown title'", 'max_length': '1024'}),
+            'completed_at': ('django.db.models.fields.DateTimeField', [], {'null': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'item_id': ('django.db.models.fields.CharField', [], {'max_length': '64', 'db_index': 'True'}),
+            'job_id': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '64', 'db_index': 'True'}),
+            'retry_number': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
+            's3_path': ('django.db.models.fields.CharField', [], {'max_length': '2048', 'null': 'True'}),
+            'started_at': ('django.db.models.fields.DateTimeField', [], {}),
+            'status': ('django.db.models.fields.CharField', [], {'max_length': '64'}),
+            'user_email': ('django.db.models.fields.CharField', [], {'default': "'Unknown user'", 'max_length': '1024', 'db_index': 'True'})
+        },
+        'gnmatomresponder.pacformxml': {
+            'Meta': {'object_name': 'PacFormXml'},
+            'atom_id': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '64', 'db_index': 'True'}),
+            'celery_task_id': ('django.db.models.fields.CharField', [], {'max_length': '64', 'null': 'True', 'db_index': 'True'}),
+            'completed': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'last_error': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'pacdata_url': ('django.db.models.fields.CharField', [], {'max_length': '4096'}),
+            'received': ('django.db.models.fields.DateTimeField', [], {}),
+            'status': ('django.db.models.fields.CharField', [], {'default': "'UNPROCESSED'", 'max_length': '32', 'db_index': 'True'})
+        }
+    }
+
+    complete_apps = ['gnmatomresponder']

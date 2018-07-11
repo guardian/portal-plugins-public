29658cbd06150260e8e0bed1833de9a7ebbea535
Andy Gallagher <andy.gallagher@theguardian.com>
2018-07-11 12:33:29 +0100

    adding other deploy steps from old

--- upload_with_hash.sh
+++ upload_with_hash.sh
@@ -1,10 +1,11 @@
 #!/bin/bash -e
 
-if [ -x `which shasum` ]; then
-    SHASUM="`which shasum` -a 256"
-else
-    SHASUM="`which sha256sum`"
-fi
+#if [ -x `which shasum` ]; then
+#    SHASUM="`which shasum` -a 256"
+#else
+#    SHASUM="`which sha256sum`"
+#fi
+SHASUM=/usr/bin/sha256sum
 
 SHA=$(${SHASUM} $1  | cut -f 1 -d ' ')
 echo SHA-256 checksum is ${SHA}
